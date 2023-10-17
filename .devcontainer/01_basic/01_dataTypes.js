aa3525d2d95fd20af8e6d09660a11407852498ec
rajoraNihal <139540529+rajoraNihal@users.noreply.github.com>
2023-10-17 06:43:29 +0000

    dataTypes

---
+++ .devcontainer/01_basic/01_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // new js code
+
+// alert(3 + 3). this alert will not work because this is node not browser 
+
+// number
+// bigInt
+// string
+// undefine => 
+// null => 
+// symbol => unique 
+
+let age = 23
+let name = "Nihal"
+
+console.table([age , name])
