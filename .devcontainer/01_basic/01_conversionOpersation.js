d29d8d381c403dfcd7f2440e1bd0509f5811760c
rajoraNihal <139540529+rajoraNihal@users.noreply.github.com>
2023-10-17 14:03:51 +0000

    conversion done

---
+++ .devcontainer/01_basic/01_conversionOpersation.js
@@ -0,0 +1,13 @@
+let age = "33"
+let newAge = Number(age)
+// console.log(newAge)
+// console.log(typeof(newAge));
+
+let x = 5
+let y = x++
+console.log(x , y);
+
+let a = 5
+let b = ++a
+
+console.log(a , b);
